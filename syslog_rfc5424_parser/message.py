324e3be03eecece10624325a4868fdc5dfccd8a7
Eyal Leshem <eyal.leshem@mbp-172754.local>
2018-05-15 16:50:38 +0300

    parse crashes if message dict contain an empty value

--- syslog_rfc5424_parser/message.py
+++ syslog_rfc5424_parser/message.py
@@ -110,7 +110,12 @@ class SyslogMessage(object):
                 sd.setdefault(item['sd_id'], {})
                 if 'sd_params' in item:
                     for param_pair in item['sd_params']:
-                        sd[item['sd_id']][param_pair['param_name']] = param_pair['param_value']
+                        try:
+                            # empty string values not appeared in the ParseResults
+                            param_value = param_pair['param_value']
+                        except KeyError:
+                            param_value = ""
+                        sd[item['sd_id']][param_pair['param_name']] = param_value
         return cls(severity=severity, facility=facility, version=version, hostname=hostname,
                    timestamp=timestamp, appname=appname, procid=procid, msgid=msgid, msg=msg,
                    sd=sd)
